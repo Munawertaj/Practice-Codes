ab3ee72e76aa7662a59b467008dd3bc82b5fb3f3
Tareq Munawer Taj <taj4580@gmail.com>
2025-04-07 20:06:44 +0600

    multiple abstract method added

--- AbstractPractice/src/Main.java
+++ AbstractPractice/src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         Shape circle = new Circle(3.0);
-        
-        circle.displayArea();
+        circle.displayInfo();
     }
 }
--- AbstractPractice/src/Shape.java
+++ AbstractPractice/src/Shape.java
@@ -1,7 +1,9 @@
 public abstract class Shape {
     public abstract double getArea();
+    public abstract double getPerimeter();
 
-    public void displayArea() {
+    public void displayInfo() {
         System.out.println("Area: " + getArea());
+        System.out.println("Perimeter: " + getPerimeter());
     }
 }
